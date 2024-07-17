54476ebdbc16b1fb6f32d1cec3510c1dd87be94f
Benedetta-Barbone <barbone1912@gmail.com>
2024-07-17 17:40:56 +0200

    create model e controller

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,21 @@
 @section('page-title', 'homepage')
 
 @section('main-content')
-    <h1>
-        Homepage
-    </h1>
+    <div class="container mt-5">
+        <div class="row">
+            @foreach($movies as $movie)
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ $movie->original_title }}</h6>
+                        <p class="card-text">Nationality: {{ $movie->nationality }}</p>
+                        <p class="card-text">Release Date: {{ $movie->date }}</p>
+                        <p class="card-text">Vote: {{ $movie->vote }}</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
